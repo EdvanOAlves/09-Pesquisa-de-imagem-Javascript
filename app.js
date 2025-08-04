6328c678f2ee8afd57a9401f070582b5f9f6663c
EDVAN ALVES DE OLIVEIRA <25132698@dsn127m1242373.cfp127aluno.local>
2025-08-04 11:28:40 -0300

    Testando solucoes para multiplas extensoes
    iniciando codigo alternativo para abrir arquivo de outras extensoes

--- app.js
+++ app.js
@@ -2,10 +2,25 @@
 
 const buttonImage = document.getElementById('update-image')
 
+const extensoes =['.jpg', '.jpeg', '.png', '.gif', '.bmp']
+
+function findExt(imageOption){
+    for (let ext in extensoes){
+
+        
+        return ext
+    }
+}
+
 function searchImage(){
     const imageOption = document.getElementById('input').value
-    const imagePath = '\"./img/'+imageOption+'.jpg\"'
+    findExt(imageOption)
+
+
+    const imagePath = `\"./img/'${imageOption}${extensoes}\"`
     console.log(imagePath)
     document.documentElement.style.setProperty( '--image-path', imagePath)
 }
+
+
 buttonImage.addEventListener('click',searchImage)
