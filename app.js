b3bfeb243e9669624e722972e4483174609043c5
unknown <edvan.aoliveira324@gmail.com>
2025-08-04 22:04:56 -0300

    Botão imagem escolhendo as imagens
    Substituído código de caminho das imagens para resolver a questão da extensão, substituindo as imagens por alguns desenhos antigos meus :)

--- app.js
+++ app.js
@@ -2,24 +2,12 @@
 
 const buttonImage = document.getElementById('update-image')
 
-const extensoes =['.jpg', '.jpeg', '.png', '.gif', '.bmp']
-
-function findExt(imageOption){
-    for (let ext in extensoes){
-
-        
-        return ext
-    }
-}
-
 function searchImage(){
-    const imageOption = document.getElementById('input').value
-    findExt(imageOption)
+    const imageInput = document.getElementById('input').value
+    const imageOption = imageInput.replace(' ','-')
 
 
-    const imagePath = `\"./img/'${imageOption}${extensoes}\"`
-    console.log(imagePath)
-    document.documentElement.style.setProperty( '--image-path', imagePath)
+    document.documentElement.style.setProperty( '--selected-image', `var(--${imageOption})` )
 }
 
 
