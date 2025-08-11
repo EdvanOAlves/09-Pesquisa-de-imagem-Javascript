9d95e1ba9f3347dc77544fde1af0e38e1afbb3b4
EDVAN ALVES DE OLIVEIRA <25132698@dsn127m1242373.cfp127aluno.local>
2025-08-11 08:24:54 -0300

    [func] iniciando funcionalidade botao auto
    faltando repeticao automatica

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 "use strict"
 
 const buttonImage = document.getElementById('update-image')
+const buttonAuto = document.getElementById('auto-slideshow')
+const slideTime = 3000
 
-function searchImage(){
+function searchImage() {
     const imageInput = document.getElementById('input').value
-    const imageOption = imageInput.replace(' ','-')
+    const imageOption = imageInput.replace(' ', '-')
 
-
-    document.documentElement.style.setProperty( '--selected-image', `var(--${imageOption})` )
+    document.documentElement.style.setProperty('--selected-image', `var(--${imageOption})`)
 }
 
+function slideShow() {
+    for (let i = 0; i < 10; i++) {
+        setTimeout(function changeImg() {
+            document.documentElement.style.setProperty('--selected-image', `var(--imagem-${i + 1})`)
+        }, slideTime * i
+        )
+    }
+}
 
-buttonImage.addEventListener('click',searchImage)
+buttonImage.addEventListener('click', searchImage)
+buttonAuto.addEventListener('click', slideShow)
